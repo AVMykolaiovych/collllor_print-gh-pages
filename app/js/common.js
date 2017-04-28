64c24c79c3d574342b02a9b8290b47bd0ad5335c
avnicolaevich <avnicolaevich@gmail.com>
2017-04-29 00:26:23 +0300

    refactor code

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $(":checkbox").labelauty();
 
+
     $('.header__sign').on('click', function (e) {
         e.preventDefault();
         $(this).parent().addClass('active');
@@ -9,4 +10,30 @@ $(document).ready(function(){
         $('.header__content > div').not(target).hide();
         $(target).fadeIn(600);
     });
+
+
+    $('#selectState').scombobox({
+        filterDelay: 300,
+        highlight: true,
+        filterIgnoreCase: true,
+        fillOnTab: true,
+        showDropDown: true,
+        placeholder: ' '
+    });
+
+    $('.header__link').click(function() {
+        var checkElement = $(this).next();
+        checkElement.stop().animate({'height':'toggle'}, 0.1, 'linear');
+        if((checkElement.is('form')) && (checkElement.is(':visible'))) {
+            return false;
+        }
+    });
+
+    $(document).click( function(event){
+        if( $(event.target).closest("form").length )
+            return;
+        $(".registration").fadeOut("0.1");
+        event.stopPropagation();
+    });
 });
+
--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,7 @@ gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/jquery-labelauty/jquery-labelauty.js',
+		'app/libs/jquery.scombobox/jquery.scombobox.min.js',
 		'app/js/common.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
