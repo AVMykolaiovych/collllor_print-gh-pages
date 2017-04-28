40589e8fe058c718df2aa2e38e0c689ea19f82e4
avnicolaevich <avnicolaevich@gmail.com>
2017-04-28 15:56:13 +0300

    refactor code

--- app/js/common.js
+++ app/js/common.js
@@ -1,47 +1,12 @@
 $(document).ready(function(){
     $(":checkbox").labelauty();
 
-    $('.header__registration').find('.header__info').on('keyup blur focus', function (e) {
-
-        var $this = $(this),
-            label = $this.prev('label');
-
-        if (e.type === 'keyup') {
-            if ($this.val() === '') {
-                label.removeClass('active highlight');
-            } else {
-                label.addClass('active highlight');
-            }
-        } else if (e.type === 'blur') {
-            if( $this.val() === '' ) {
-                label.removeClass('active highlight');
-            } else {
-                label.removeClass('highlight');
-            }
-        } else if (e.type === 'focus') {
-
-            if( $this.val() === '' ) {
-                label.removeClass('highlight');
-            }
-            else if( $this.val() !== '' ) {
-                label.addClass('highlight');
-            }
-        }
-
-    });
-
     $('.header__sign').on('click', function (e) {
-
         e.preventDefault();
-
         $(this).parent().addClass('active');
         $(this).parent().siblings().removeClass('active');
-
         target = $(this).attr('href');
-
         $('.header__content > div').not(target).hide();
-
         $(target).fadeIn(600);
-
     });
 });
